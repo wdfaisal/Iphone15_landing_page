cf13ae9bac8c182e55d0c4352d33920eb5206b10
wdfaisal <a7medfaisal7@gmail.com>
2024-06-15 02:18:03 +0100

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,26 +6,13 @@ const Footer = () => {
     <footer className="py-5 sm:px-10 px-5">
       <div className="screen-max-width">
         <div>
-          <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
-          </p>
-          <p className="font-semibold text-gray text-xs">
-            Or call 000800-040-1966
-          </p>
+          
         </div>
 
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved. devolped by Ahmed Faisal</p>
           <div className="flex">
             {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
